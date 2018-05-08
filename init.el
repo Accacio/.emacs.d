429a8a23f15f5e5d926ad1941dd6c716e27f7708
Accacio <raccacio@poli.ufrj.br>
2018-05-09 01:56:57 +0200

    EXWM sublimity and other tweaks greeeat :)

--- init.el
+++ init.el
@@ -26,13 +26,35 @@
        (string= window-system "w32")
        (quote "d:/Users/Accacio/AppData/Local/Programs/Python/Python36-32/Scripts/cpplint.exe")
      (quote "/usr/local/bin/cpplint")))
+ '(ledger-reports
+   (quote
+    (("reg" "ledger [[ledger-mode-flags]] -f /home/accacio/.emacs.d/ledger.ledger reg")
+     (#("bal" 0 1
+	(idx 0))
+      "%(binary) -f %(ledger-file) bal")
+     (#("payee" 0 1
+	(idx 2))
+      "%(binary) -f %(ledger-file) reg @%(payee)")
+     (#("account" 0 1
+	(idx 3))
+      "%(binary) -f %(ledger-file) reg %(account)"))))
+ '(minimap-dedicated-window t)
+ '(minimap-hide-scroll-bar nil)
+ '(minimap-mode nil)
+ '(minimap-recreate-window nil)
+ '(minimap-window-location (quote left))
+ '(neo-autorefresh t)
+ '(neo-click-changes-root t)
+ '(neo-confirm-change-root (quote off-p))
+ '(neo-force-change-root t)
+ '(neo-smart-open t)
  '(org-hide-leading-stars nil)
  '(package-selected-packages
    (quote
-    (counsel-projectile projectile pdf-tools helm-company atomic-chrome beacon flx-ido smex helm-gtags rainbow-mode irony-eldoc expand-region multiple-cursors neotree cmake-mode fill-column-indicator idle-highlight-mode idle-highligh-mode ox-reveal smartparens company-irony-c-headers company-irony irony irony-mode nyan-mode company flymake-cursor google-c-style flymake-google-cpplint flycheck undo-tree yasnippet yasnippet-snippet yasnippets iedit magit which-key use-package try tabbar org-bullets counsel ace-window))))
+    (ledger-mode ace-mc sublimity counsel-projectile projectile pdf-tools helm-company atomic-chrome beacon flx-ido smex helm-gtags rainbow-mode irony-eldoc expand-region multiple-cursors neotree cmake-mode fill-column-indicator idle-highlight-mode idle-highligh-mode ox-reveal smartparens company-irony-c-headers company-irony irony irony-mode nyan-mode company flymake-cursor google-c-style flymake-google-cpplint flycheck undo-tree yasnippet yasnippet-snippet yasnippets iedit magit which-key use-package try tabbar org-bullets counsel ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0 :background "sea green" :foreground "#000")))))
